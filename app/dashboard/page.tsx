b02c829ffffbd65b52ab217f9b9bc8705599d600
Asquare952 <abdulafeezabdulazeez894@gmail.com>
2025-12-31 21:06:01 +0100

    fix: fix dashboard text

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -28,8 +28,8 @@ export default function page() {
       <section className=" flex flex-col gap-6 p-3">
         <div className=" flex justify-between">
           <div className=" flex flex-col gap-0.5">
-            <h1 className=" text-[25px] font-bold md:text-[32px] text-[#162B4C]">Welcome,Daniel</h1>
-            <p className=" font-normal text-[16px] text-[#3D3F42]">Here is how your properties are performing today</p>
+            <h1 className=" text-[20px] font-bold md:text-[32px] text-[#162B4C]">Welcome, Daniel</h1>
+            <p className=" font-normal text-[12px] md:text-[16px] text-[#3D3F42]">Here is how your properties are performing today</p>
           </div>
 
           <form>
@@ -51,7 +51,7 @@ export default function page() {
                     <Image src={image} width={32} height={32} alt="" className=" bg-[#43A047] p-1 text-white rounded" />
                     <h3>{name}</h3>
                   </div>
-                  <p className=" font-bold md:text-4xl text-[#162B4C]">{figure}</p>
+                  <p className=" font-bold  xl:text-4xl text-[#162B4C]">{figure}</p>
                 </div>
                 <div className="flex items-center gap-1">
                   <p className=" bg-[#D9ECDA] text-[#43A047] py-1 px-3 rounded-2xl">+{percentage}</p>
@@ -100,7 +100,7 @@ export default function page() {
                         <td className=" flex gap-1 py-2">
                           <Image src={image} width={80} height={32} alt=""/>
                           <div className=" flex flex-col gap-0.5">
-                            <h3 className=" font-semibold text-[15px] w-[220px] text-[#7A7E84]">{name}</h3>
+                            <h3 className=" font-semibold text-[12px] md:text-[15px] w-[220px] text-[#7A7E84]">{name}</h3>
                             <p className=" font-normal text-[10px] text-[#7A7E84]">{location}</p>
                           </div>
                         </td>
@@ -133,8 +133,8 @@ export default function page() {
           {/* inquiries */}
           <div className=" flex flex-col gap-6 bg-white p-4 rounded-[12px]">
             <div className=" flex flex-col gap-0.5">
-              <h1 className=" font-bold text-[32px] text-[#162B4C]">Recent Inquiries</h1>
-              <p className=" font-normal text-[16px] text-[#3D3F42]">See who is interested in your properties</p>
+              <h1 className=" font-bold text-[20px] md:text-[32px] text-[#162B4C]">Recent Inquiries</h1>
+              <p className=" font-normal text-[12px] md:text-[16px] text-[#3D3F42]">See who is interested in your properties</p>
             </div>
 
             <div className=" flex flex-col gap-6">
@@ -145,7 +145,7 @@ export default function page() {
                     <div className=" flex items-center gap-1">
                       <Image src={image} width={49} height={49} alt="" />
                       <div>
-                        <h3 className="font-semibold text-[15px] text-[#7A7E84]">{name}</h3>
+                        <h3 className="font-semibold text-[12px] md:text-[15px] text-[#7A7E84]">{name}</h3>
                         <p className="font-normal text-[10px] text-[#7A7E84]">{message}</p>
                       </div>
                     </div>
@@ -161,14 +161,14 @@ export default function page() {
 
           {/* income overviews */}
           <div className="flex flex-col gap-6 bg-white p-4 rounded-[12px]">
-            <div className=" flex flex-col gap-4 md:flex-row md:justify-between">
+            <div className=" flex flex-col gap-4 xl:flex-row md:justify-between">
               <div className=" flex flex-col gap-0.5">
                 <h1 className=" font-medium text-[20px] text-[#162B4C]">Income overview</h1>
                 <p className=" font-normal text-[16px] text-[#3D3F42] w-[273px]">Monitor your rental income
                   performance over time</p>
               </div>
               <div className=" flex flex-col gap-0.5">
-                <h1 className=" font-bold text-[24px] text-[#162B4C]">$20,000</h1>
+                <h1 className=" font-bold text-[20px] md:text-[24px] text-[#162B4C]">$20,000</h1>
                 <div className="flex items-center gap-1">
                   <p className=" bg-[#D9ECDA] text-[#43A047] py-1 px-3 rounded-2xl">+12%</p>
                   <p>This month</p>
